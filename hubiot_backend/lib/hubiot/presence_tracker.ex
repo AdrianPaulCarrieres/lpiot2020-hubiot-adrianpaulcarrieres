adff0e44813691092520dced33c81b2c85e8a307
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2021-04-14 21:09:46 +0200

    handled delete

--- hubiot_backend/lib/hubiot/presence_tracker.ex
+++ hubiot_backend/lib/hubiot/presence_tracker.ex
@@ -1,7 +1,7 @@
 defmodule Hubiot.PresenceTracker do
   use GenServer
 
-  #Public api
+  # Public api
   def start(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
@@ -30,6 +30,7 @@ defmodule Hubiot.PresenceTracker do
       nil ->
         state = Map.put(state, location, [name])
         {:reply, {:ok, name}, state}
+
       users when is_list(users) ->
         users = users ++ [name]
         state = Map.put(state, location, users)
@@ -43,4 +44,18 @@ defmodule Hubiot.PresenceTracker do
     {:reply, {:ok, users}, state}
   end
 
+  @impl true
+  def handle_call({:delete, location, name}, _from, state) do
+    with users when is_list(users) <- Map.get(state, location),
+         users when users != [] <- List.delete(users, name) do
+      state = Map.put(state, location, users)
+      {:reply, {:ok, name}, state}
+    else
+      nil ->
+        {:reply, {:error, "Location not found"}, state}
+      [] ->
+        state = Map.delete(state, location)
+        {:reply, :no_users_left, state}
+    end
+  end
 end
