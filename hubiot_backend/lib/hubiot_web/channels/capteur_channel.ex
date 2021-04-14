ebde63dd7d56751f29e112b7c29f552fd93aa16b
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2021-04-14 17:00:58 +0200

    adding after join channel

--- hubiot_backend/lib/hubiot_web/channels/capteur_channel.ex
+++ hubiot_backend/lib/hubiot_web/channels/capteur_channel.ex
@@ -1,14 +1,18 @@
 defmodule HubiotWeb.CapteurChannel do
   use Phoenix.Channel
+  alias Phoenix.Socket.Broadcast
 
   def join("capteur:" <> _, _message, socket) do
+    send(self(), :after_join)
     {:ok, socket}
   end
-
-  alias Phoenix.Socket.Broadcast
 
   def handle_info(%Broadcast{topic: _, event: event, payload: payload}, socket) do
     push(socket, event, payload)
     {:noreply, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    #broadcast!(socket, "new_student", %{game: game})
   end
 end
