ba7f6a6c15b62f392c8b47c92ee699c77cb6ad58
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2021-04-18 14:40:03 +0200

    added qr code reader

---
+++ HubiotApp/app/src/main/java/fr/lpiot/hubiot/Qr.java
@@ -0,0 +1,75 @@
+package fr.lpiot.hubiot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class Qr extends AppCompatActivity {
+
+    ImageView imageView;
+    Button button;
+    Button btnScan;
+    EditText editText;
+
+    TextView tv_qr_readTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        editText = (EditText) findViewById(R.id.editText);
+        btnScan = (Button) findViewById(R.id.btnScan);
+        tv_qr_readTxt = (TextView) findViewById(R.id.tv_qr_readTxt);
+
+        btnScan.setOnClickListener(view -> {
+
+            IntentIntegrator integrator = new IntentIntegrator(Qr.this);
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+            integrator.setPrompt("Scan");
+            integrator.setCameraId(0);
+            integrator.setBeepEnabled(false);
+            integrator.setBarcodeImageEnabled(false);
+            integrator.initiateScan();
+
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Log.e("Scan*******", "Cancelled scan");
+
+            } else {
+                Log.e("Scan", "Scanned");
+
+                tv_qr_readTxt.setText(result.getContents());
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+
+                navigateToHome(result.getContents());
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void navigateToHome(String location){
+        Intent intent = new Intent(this, Hub.class);
+        intent.putExtra("location", location);
+        startActivity(intent);
+    }
+}
